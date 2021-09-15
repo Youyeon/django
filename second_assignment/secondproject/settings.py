52ca57b6892c6d5b5b10a64f2e77d7bf1b5730c2
Youyeon <yourain416@gmail.com>
2021-09-15 22:30:22 +0900

    portfolio

--- second_assignment/secondproject/settings.py
+++ second_assignment/secondproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'portfolio.apps.PortfolioConfig',
     'blog.apps.BlogConfig', # add
     'django.contrib.admin',
     'django.contrib.auth',
